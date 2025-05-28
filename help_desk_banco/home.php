6c338ed49e5827feee7ef46a3ccb12dfd04ce708
Victor Boy <victor2005boy@gmail.com>
2025-05-28 11:56:28 -0300

    certo

--- help_desk_banco/home.php
+++ help_desk_banco/home.php
@@ -1,4 +1,3 @@
-
 <?php
 require_once "validador_acesso.php";
 //require
@@ -10,70 +9,108 @@ require_once "validador_acesso.php";
 
 
 <html>
-  <head>
-    <meta charset="utf-8" />
-    <title>App Help Desk</title>
 
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+<head>
+  <meta charset="utf-8" />
+  <title>App Help Desk</title>
 
-    <style>
-      .card-home {
-        padding: 30px 0 0 0;
-        width: 100%;
-        margin: 0 auto;
-      }
-      button{
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
+    integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 
-        border-radius:5px ;
-        color:'white';
-      }
-      button:hover{
+  <style>
+    .card-home {
+      padding: 30px 0 0 0;
+      width: 100%;
+      margin: 0 auto;
+    }
 
-        color:'white';
-         background-color:  #343A40 ;
-         color:'#ffff';
-      }
-      
-    </style>
-  </head>
+    button {
 
-  <body>
+      border-radius: 5px;
+      color: 'white';
+    }
 
-    <nav class="navbar navbar-dark bg-dark">
-      <a class="navbar-brand" href="#">
-        <img src="logo.png" width="30" height="30" class="d-inline-block align-top" alt="">
-        App Help Desk
-      </a>
-      <div>
-       <a href="index.php">
-        <button>Sair</button> </a> 
-      </div>
-    </nav>
+    button:hover {
 
-    <div class="container">    
-      <div class="row">
+      color: 'white';
+      background-color: #343A40;
+      color: '#ffff';
+    }
+  </style>
+</head>
 
-        <div class="card-home">
-          <div class="card">
-            <div class="card-header">
-              Menu
-            </div>
-            <div class="card-body">
-              <div class="row">
-                <div class="col-6 d-flex justify-content-center">
-                  <a href="abrir_chamado.php">
+<body>
+
+  <nav class="navbar navbar-dark bg-dark">
+    <a class="navbar-brand" href="#">
+      <img src="logo.png" width="30" height="30" class="d-inline-block align-top" alt="">
+      App Help Desk
+    </a>
+    <div>
+      <a href="index.php">
+        <button>Sair</button> </a>
+    </div>
+  </nav>
+
+  <div class="container">
+    <div class="row">
+
+      <div class="card-home">
+        <div class="card">
+          <div class="card-header">
+            Menu
+          </div>
+          <div class="card-body">
+            <div class="row">
+
+
+              <div class="col-2 d-flex justify-content-center">
+                <a href="abrir_chamado.php">
                   <img src="formulario_abrir_chamado.png" width="70" height="70">
-                  </a>
-                </div>
-                <div class="col-6 d-flex justify-content-center">
-                  <a href="consultar_chamado.php">
-                  <img src="formulario_consultar_chamado.png" width="70" height="70">
-                  </a>
-                </div>
+                </a>
               </div>
+
+
+              <div class="col-2 d-flex justify-content-center">
+                <a href="abrir_chamado.php">
+                  <img src="formulario_abrir_chamado.png" width="70" height="70">
+                </a>
+              </div>
+
+              <div class="col-2 d-flex justify-content-center">
+                <a href="abrir_chamado.php">
+                  <img src="formulario_abrir_chamado.png" width="70" height="70">
+                </a>
+              </div>
+
+              <div class="col-2 d-flex justify-content-center">
+                <a href="abrir_chamado.php">
+                  <img src="formulario_abrir_chamado.png" width="70" height="70">
+                </a>
+              </div>
+
+              <div class="col-2 d-flex justify-content-center">
+                <a href="abrir_chamado.php">
+                  <img src="formulario_abrir_chamado.png" width="70" height="70">
+                </a>
+              </div>
+
+                            <div class="col-2 d-flex justify-content-center">
+                <a href="abrir_chamado.php">
+                  <img src="formulario_abrir_chamado.png" width="70" height="70">
+                </a>
+              </div>
+
+
+
+
+
             </div>
           </div>
         </div>
+      </div>
     </div>
-  </body>
+  </div>
+</body>
+
 </html>
---
+++ help_desk_banco/valida_cadastro.php
@@ -0,0 +1,65 @@
+
+<?php
+
+    include ('config.php');
+    //  verificar se o cadastro ja existe  
+    $sql= "SELECT * FROM usuarios WHERE email='{$_POST['email']}'";
+    $res= $conexao->query($sql);
+
+
+
+
+
+    if($res->num_rows >0){
+
+        header('location: cadastro.php?cadastro=erro');
+        exit();
+    }
+
+    if($_POST['perfil'] === "-- Selecione --")
+    {
+    header('location: cadastro.php?validaperfil=erro');
+    }
+    
+    else{
+
+    $nome   =   $_POST['nome'];
+    $email  =   $_POST['email'];
+    $senha  =   md5($_POST['senha']);
+    $perfil =   $_POST['perfil'];
+
+    $sql = "INSERT INTO usuarios(nome, email, senha, perfil) VALUE('{$nome}',  '{$email}',  '{$senha}',  '{$perfil}')";
+    $res =  $conexao -> query($sql);
+    
+    if($res ==true){
+    header('location: index.php?usuario=sucesso');
+    }
+
+    else{
+        header('location: index.php?usuario=falha');
+    }
+}
+?>
+
+
+
+<!-- slq
+res   conn
+
+if se alterou mais de uma linha 
+se alterou mande erro e extit
+
+se foi 
+verificar se o perfil é igualo ao seleciona?  //nao poder ficar em branco
+
+se não 
+cadastre  todas variaveis com nomes usando post (não esqueça a codificação da senha)
+
+sql insert into  valores em oredem
+
+abrir a coneção
+
+
+se foir res true 
+usuario cusssedso 
+usuario falhalk -->
